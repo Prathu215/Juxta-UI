ce184762bd1b96d892555f9077152e2095c46c45
Prathyusha Vinukonda <prathyusha.system@gmail.com>
2018-04-01 14:19:25 -0400

    Categories modified

--- src/app/electronics-list/electronics-list.component.ts
+++ src/app/electronics-list/electronics-list.component.ts
@@ -10,6 +10,9 @@ import { FormGroup, FormBuilder } from '@angular/forms';
 export class ElectronicsListComponent implements OnInit {
 
   electronics:any[];
+  isElectronicsExists:boolean=true;
+  dell:any[]=[];
+  hp:any[]=[];
   electronicsListForm:FormGroup;
 
   constructor(private electronicsList:ElectronicsListService,private fb:FormBuilder) { }
@@ -18,8 +21,27 @@ export class ElectronicsListComponent implements OnInit {
     this.electronicsList.getElectronics().subscribe(res=>{
       console.log(res);
       this.electronics=res;
+      for(let i=0;i<this.electronics.length;i++){
+        console.log(res[i].brand);
+        if(res[i].brand=="dell"){
+            console.log(res[i],"printing dell");
+            this.dell.push(res[i]);
+        }
+        if(res[i].brand=="hp"){
+          console.log(res[i],"printing hp");
+          this.hp.push(res[i]);
+      }
+        
+      
+      }
+      console.log(this.dell,"dell array");
+      console.log(this.hp,"printing hp");
+      if(this.electronics==null){
+        this.isElectronicsExists=!this.isElectronicsExists;
+      }
       console.log(this.electronics);
-    });
+    },
+  err=>alert("server not running"));
     this.electronicsListForm=this.fb.group({
 
     })
